7103015504568fab0fc50e3d061382d476c31e11
alpzaf <alialpzafer@gmail.com>
2022-03-22 15:18:57 +0300

    project finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
+import {CountryType} from "./types";
+import Country from "./components/Country";
+import Loading from "./components/Loading";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ countries, setCountries ] = useState<CountryType[]>([])
+    const [ loading, setLoading ] = useState<boolean>(false)
+
+    const getCountries = async () => {
+        setLoading(true)
+        try {
+            const { data } = await axios.get<CountryType[]>('http://api.countrylayer.com/v2/all?access_key=e7ea5787c3d9ee5a7578afcb00d9102f')
+            setCountries(data)
+        }
+        catch {
+            console.log('An Error Occured While Fetching Data')
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        getCountries()
+    },[])
+
+    return (
+        <Loading loading={loading}>
+            {countries.map(country => {
+                return (
+                    <Country key={country.name} country={country} />
+                )
+            })}
+        </Loading>
+    );
 }
 
 export default App;
